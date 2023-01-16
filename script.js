a9ca5debc572736e3c91f23a1354ddae6d44ed70
kitrath <crathmel@gmail.com>
2023-01-16 17:15:39 -0600

    save time-block content in localStorage

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
+  $('.saveBtn').click(function (event) {
+    const parentDiv = $( this ).parent();
+    const textArea = parentDiv.find('textarea');
+    localStorage.setItem(parentDiv.attr('id'), textArea.val().trim());
+  })
+
+  // Apply the past, present, or future class to each time
+  // block by comparing the id to the current hour.   
+
   // idHour: number - returns string className
   // one of 'past', 'present', or 'future'
   function getTimeComparisonClass(idHour) {
