61b9917b55f14cedde07dc6b07266713d6e74c00
kitrath <crathmel@gmail.com>
2023-01-16 17:50:37 -0600

    remove TODOs and extra comments

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Run code after DOM rendered.
 $(function () {
 
   // Get current date/time
@@ -27,9 +25,6 @@ $(function () {
 
   // Apply the past, present, or future class to each time
   // block by comparing the id to the current hour.   
-
-  // idHour: number - returns string className
-  // one of 'past', 'present', or 'future'
   function getTimeComparisonClass(idHour) {
     
     const nowHour = now.hour(); 
@@ -69,12 +64,8 @@ $(function () {
 
   });
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  // TODO: Add code to display the current date in the header of the page.
+  // Display current time at top of app.
   const fullDateFormatString = 'dddd[,] MMMM D[,] YYYY';
   $('#currentDay').text(now.format(fullDateFormatString));
-});
 
+});
