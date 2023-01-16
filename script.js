1a5883df116e1083ae786b634b669f62090e2d13
kitrath <crathmel@gmail.com>
2023-01-16 12:08:30 -0600

    add display current day at top of calendar

--- script.js
+++ script.js
@@ -20,4 +20,8 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  const now = dayjs();
+  const fullDateFormatString = 'dddd[,] MMMM D[,] YYYY';
+  $('#currentDay').text(now.format(fullDateFormatString));
 });
+
